8e67fb54df323084ee6c5f41771463def2eaf271
osantonio <jlant1991@gmail.com>
2022-04-27 01:01:47 -0500

    preparando para heroku

--- components/navbar2.vue
+++ components/navbar2.vue
@@ -4,14 +4,13 @@
     <nav class="sticky top-0">
       <header class="w-full px-10 bg-black text-white h-20 shadow-2xl flex justify-between items-center ">
         <div class="text-5xl">
-          <adatar/>
+         <a href="http://localhost:3000"><adatar/></a>
         </div>
         <div class="text-center quicksand">
-          <button>
             <a href="login">
           <p class="text-2xl"> <a class="text-green-500">A</a>nálisis de <a class="text-purple-500">D</a>atos <a class="text-green-500">A</a>cadémicos para</p>
          <p class="text-2xl"> <a class="text-blue-500">T</a>empranas <a class="text-green-500">A</a>lertas Sobre <a class="text-purple-500">R</a>etención</p>
-        </a></button>
+        </a>
         </div>
         <div class="text-3xl flex">
           <unicorlogo />
