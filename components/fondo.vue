21da2b0d1e46b46538264ba212fd13526ba89b86
osantonio <jlant1991@gmail.com>
2022-04-26 23:56:03 -0500

    adatar

---
+++ components/fondo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <img class="fondo-login" src="img/fondo.jpg" alt="">
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+<style>
+.fondo-login{
+    height: 84.5vh;
+     width: 100%;
+}
+</style>
